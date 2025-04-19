c757010c5db62db444f20cec9751228f2c133bd8
Aesha <swatidutta993@gmail.com>
2025-04-19 16:57:10 +0545

    Make separate pydantic class for request and response

--- veel_internship/schemas/pydantic_schema.py
+++ veel_internship/schemas/pydantic_schema.py
@@ -7,17 +7,17 @@ class RequestRecipe(BaseModel):
     Food_Name: str = Field(..., description="Name of the food")
     Ingredients: List[str] = Field(..., description ="List of foods")
 
-    model: str = Field(default = "qwen")
-    system_prompt: str = Field(default = "You are a helpful assistant that generates food recipes.")
-    temperature: float = Field(default = 0.5)
-    top_p: float = Field(default = 0.9)
+    # model: str = Field(default = "qwen")
+    # system_prompt: str = Field(default = "You are a helpful assistant that generates food recipes.")
+    # temperature: float = Field(default = 0.5)
+    # top_p: float = Field(default = 0.9)
 
 class Responserecipe(BaseModel):
     title: str = Field(..., description="Title of the recipe")
     ingredients: List[str] = Field(..., description="List of ingredients")
     steps: List[str] = Field(..., description="List of steps to make the recipe")
 
-    temperature: float = Field(default = 0.5)
-    top_p: float = Field(default = 0.9)
+    # temperature: float = Field(default = 0.5)
+    # top_p: float = Field(default = 0.9)
 
 
