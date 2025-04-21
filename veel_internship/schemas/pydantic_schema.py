b35f86fe9b716880704242201780d7c1816fa4af
Aesha <swatidutta993@gmail.com>
2025-04-21 08:49:03 +0545

    import logging

--- veel_internship/schemas/pydantic_schema.py
+++ veel_internship/schemas/pydantic_schema.py
@@ -1,3 +1,5 @@
+import logging
+from configs.logging_config import setup_logging
 from typing import List
 from pydantic import BaseModel, Field
 from typing import Literal
@@ -7,17 +9,9 @@ class RequestRecipe(BaseModel):
     Food_Name: str = Field(..., description="Name of the food")
     Ingredients: List[str] = Field(..., description ="List of foods")
 
-    # model: str = Field(default = "qwen")
-    # system_prompt: str = Field(default = "You are a helpful assistant that generates food recipes.")
-    # temperature: float = Field(default = 0.5)
-    # top_p: float = Field(default = 0.9)
-
 class Responserecipe(BaseModel):
     title: str = Field(..., description="Title of the recipe")
     ingredients: List[str] = Field(..., description="List of ingredients")
     steps: List[str] = Field(..., description="List of steps to make the recipe")
-
-    # temperature: float = Field(default = 0.5)
-    # top_p: float = Field(default = 0.9)
 
 
