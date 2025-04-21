d0f980e9bcafc51ad6f3b9c08c518f376da6cd6a
Aesha <swatidutta993@gmail.com>
2025-04-21 11:40:05 +0545

    Necessary changes

--- veel_internship/main.py
+++ veel_internship/main.py
@@ -1,19 +1,19 @@
-
-
-import logging	
-from configs.logging_config import setup_logging
-from models.ollama_model import OllamaModel
-from schemas.pydantic_schema import RequestRecipe  # importing from file from the root directory
-from prompts.prompt_templates import SystemPrompts
+import logging
 import json
 
+from configs.logging_config import setup_logging
+from models.ollama_model import OllamaModel
+from schemas.pydantic_schema import RequestRecipe
+from prompts.prompt_templates import SystemPrompts
+
 if __name__ == "__main__":
-    new_model = OllamaModel(model_name="qwen")  # model name
+    setup_logging()
+    new_model = OllamaModel(model_name="qwen")
 
     schema = RequestRecipe.model_json_schema()
     logging.info("Generating Recipe")
 
-    # Pretty print the JSON schema
+    # Print in JSON
     print(json.dumps(schema, indent=2))
 
     input_json = {
@@ -22,13 +22,7 @@ if __name__ == "__main__":
         "Ingredients": ["rice", "egg", "peas", "carrot", "soy sauce"]
     }
 
-    stream = True  # Toggle streaming
+    # Streaming defined by the method itself
+    recipe = new_model.generate_recipe(input_json, stream=True)
 
-    
-if stream:
-    recipe = new_model.streaming(input_json)
-    
-logging.info("Final recipe output:\n" + recipe)
-
-
-   
+    logging.info("Final recipe output:\n" + recipe)
